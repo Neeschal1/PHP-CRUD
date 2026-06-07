82f8c651a88c444ba4129c4525fa8b98fbedf958
Nees C Hal <neeschalpok04@gmail.com>
2026-06-07 20:47:43 +0545

    Refactor database connection code for clarity

--- db.php
+++ db.php
@@ -1,5 +1,11 @@
 <?php
-$conn = mysqli_connect("127.0.0.1", "root", "root12345", "student_db");
+
+$host = "127.0.0.1";
+$username = "root";
+$password = "root12345";
+$database = "student_db";
+
+$conn = mysqli_connect($host, $username, $password, $database);
 
 if (!$conn) {
     die("Connectiondsd failed: " . mysqli_connect_error());
